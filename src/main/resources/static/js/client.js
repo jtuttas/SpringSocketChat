c1aafd4abbfd76f0ca50ffd1bb1aa7d17ac5be9b
jtuttas <jtuttas@gmx.net>
2023-01-06 08:52:17 +0100

    V0.14

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -2,26 +2,32 @@ let ws;
 
 $(document).ready(function () {
   $("#anmelden").click(function (e) {
-    console.log("Connect to " + window.location.hostname+" over "+location.protocol);
-    if (location.protocol=="https") {
-        ws = new WebSocket("wss://" + location.host + "/chat");
-    }
-    else {
-        ws = new WebSocket("ws://" + location.host + "/chat");
+    console.log(
+      "Connect to " + window.location.hostname + " over " + location.protocol
+    );
+    if (location.protocol == "https:") {
+      ws = new WebSocket("wss://" + location.host + "/chat");
+    } else {
+      ws = new WebSocket("ws://" + location.host + "/chat");
     }
     ws.onmessage = function (e) {
       console.log("onmessage()" + JSON.stringify(e.data));
       printMessage(e.data);
     };
-    ws.onopen = function() {
-        let messageObject = {
-          name: "system",
-          message: $("#user").val()+" joined the Chat",
-        };
-        ws.send(JSON.stringify(messageObject));
-    }
+    ws.onopen = function () {
+      let messageObject = {
+        name: "system",
+        message: $("#user").val() + " joined the Chat",
+      };
+      ws.send(JSON.stringify(messageObject));
+    };
     $("#logindiv").addClass("hidden");
     $("#chatdiv").removeClass("hidden");
+  });
+
+  $("#send").click(function (e) { 
+    console.log('Sende '+$("#inputsm").val());
+    sendToGroupChat();    
   });
 });
 
@@ -39,20 +45,25 @@ function connect() {
 
 function printMessage(data) {
   let messageData = JSON.parse(data);
-  $("#messages").append("<p>"+messageData.name + " : " + messageData.message+"</p>");
-
-  /*
-  let messages = document.getElementById("messages");
-  let newMessage = document.createElement("div");
-  newMessage.innerHTML = messageData.name + " : " + messageData.message;
-  messages.appendChild(newMessage);
-  */
+  if (messageData.name == "system") {
+    $("#messages").append('<p class="system">' + messageData.message + "</p>");
+  } else if (messageData.name == $("#user").val()) {
+    $("#messages").append('<p class="self">' + messageData.message + "</p>");
+  } else {
+    $("#messages").append(
+      '<p class="chatline"><span class="from">' +
+        messageData.name +
+        '</span><span class="frommsg">' +
+        messageData.message +
+        "</span></p>"
+    );
+  }
 }
 
 function sendToGroupChat() {
-  let messageText = document.getElementById("message").value;
-  document.getElementById("message").value = "";
-  let name = document.getElementById("name").value;
+  let messageText =  $("#inputsm").val();
+  $("#inputsm").val("");
+  let name = $("#user").val();
   let messageObject = {
     name: name,
     message: messageText,
